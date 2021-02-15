91d8ef6d1b2038b012bd5a2fa0680d88a6f8786c
Andy Blankley <andrew.blankley@hotmail.com>
2021-02-15 21:41:27 +0000

    Visualized the data further
    Added Seaborn and Matplot to visualize the data. Using the HeatMap and the pairplot, the data shows conclusive results about which variables are correlated strongly in both positive and negative directions.

--- AppliedScripting/Assigment2_DataDiscovery/regression.py
+++ AppliedScripting/Assigment2_DataDiscovery/regression.py
@@ -6,6 +6,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 
+import seaborn as sns
 
 ## File input via CSV
 try:
@@ -27,12 +28,39 @@ print("Data Frame component details (describe()):")
 print(data.describe())
 print("Standard Deviations:")
 print(data.std())
-
-print()
+print("Correlation between values:")
+print(data.corr())
 
 ## Begin modeling
 
+# Our dependent variable is the netenergeyoutput of the DF.
+y = data.NetEnergyOutput
 
+
+# Visualization
+sns.displot(y, bins=30)
+plt.show()
+
+ax = sns.heatmap(data.corr(),
+cmap=sns.cubehelix_palette(20, light=0.95, dark=0.15))
+ax.xaxis.tick_top() # move labels to the top
+
+sns.pairplot(data,
+plot_kws={'alpha': 0.6},
+diag_kws={'bins': 30})
+plt.show()
+
+# The visualization indicates that we have correlation between the following:
+# NEO and Temperature
+# AmbientPressure and Temp
+# NEO  and AmbientPressure
+####### Results
+#                  Temperature  AmbientPressure  ...  ExhaustVacuum  NetEnergyOutput
+# Temperature         1.000000         0.842456  ...      -0.549027        -0.949082
+# AmbientPressure     0.842456         1.000000  ...      -0.309473        -0.866988
+# RelHumidity        -0.480354        -0.386454  ...       0.081330         0.499157
+# ExhaustVacuum      -0.549027        -0.309473  ...       1.000000         0.391671
+# NetEnergyOutput    -0.949082        -0.866988  ...       0.391671         1.000000
 
 
 
